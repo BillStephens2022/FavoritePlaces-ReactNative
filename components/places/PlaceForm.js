1c1d64021488e0d66b35ab693e2fa6d453e39482
BillStephens2022 <two4onebill@yahoo.com>
2023-12-11 07:24:39 -0500

    added a placeholder for the PlaceForm component for adding new Places and returned it in the AddPlace screen

---
+++ components/places/PlaceForm.js
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function PlaceForm() {
+    return <View>
+        <Text>The Place Form</Text>
+    </View>
+}
+
+export default PlaceForm;
+
+const styles = StyleSheet.create({});
