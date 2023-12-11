89295117aac731f45a99a2fc085190794c6df711
BillStephens2022 <two4onebill@yahoo.com>
2023-12-11 07:54:09 -0500

    updated titles for the AllPlaces and AddPlace screens, added a color palate of color variables in a new constants folder

--- App.js
+++ App.js
@@ -18,12 +18,15 @@ export default function App() {
             name="AllPlaces" 
             component={AllPlaces} 
             options={({navigation}) => ({
+            title: "Your Favorite Places",
             headerRight: ({ tintColor }) => (
               <IconButton icon="add" size={24} color={tintColor} onPress={() => navigation.navigate("AddPlace")} />
               ),
           })}
           />
-          <Stack.Screen name="AddPlace" component={AddPlace} />
+          <Stack.Screen name="AddPlace" component={AddPlace} options={{
+            title: "Add a new Place"
+          }}/>
         </Stack.Navigator>
       </NavigationContainer>
     </>
