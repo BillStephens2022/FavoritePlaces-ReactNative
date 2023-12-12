59f1cfbca18dee595fa83915d7379326a3a1a3a8
BillStephens2022 <two4onebill@yahoo.com>
2023-12-12 09:03:04 -0500

    added a util function to get the address of the saved location using google reverse geocoding api

--- components/places/LocationPicker.js
+++ components/places/LocationPicker.js
@@ -7,7 +7,7 @@ import {
 } from "expo-location";
 import OutlinedButton from "../ui/OutlinedButton";
 import { Colors } from "../../constants/colors";
-import { getMapPreview } from "../../util/location";
+import { getAddress, getMapPreview } from "../../util/location";
 import {
   useNavigation,
   useRoute,
@@ -34,7 +34,16 @@ function LocationPicker({ onPickLocation }) {
   }, [route, isFocused]);
 
   useEffect(() => {
-    onPickLocation(pickedLocation);
+    async function handleLocation() {
+      if (pickedLocation) {
+        const address = await getAddress(
+          pickedLocation.lat,
+          pickedLocation.lng
+        );
+        onPickLocation({...pickedLocation, address: address});
+      }
+    }
+    handleLocation();
   }, [pickedLocation, onPickLocation]);
 
   async function verifyPermissions() {
