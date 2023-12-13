eadb6f916e8643cbe9f18128ce5f86e4ea74147d
BillStephens2022 <two4onebill@yahoo.com>
2023-12-13 06:39:36 -0500

    set up expo SQLite in project with initialization of places database

--- App.js
+++ App.js
@@ -1,38 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import AllPlaces from './screens/AllPlaces';
-import AddPlace from './screens/AddPlace';
-import Map from './screens/Map';
-import IconButton from './components/ui/IconButton';
-import { Colors } from './constants/colors';
+import { useEffect, useState } from "react";
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AllPlaces from "./screens/AllPlaces";
+import AddPlace from "./screens/AddPlace";
+import Map from "./screens/Map";
+import IconButton from "./components/ui/IconButton";
+import { Colors } from "./constants/colors";
+import { init } from "./util/database";
 
 const Stack = createNativeStackNavigator();
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [dbInitialized, setDbInitialized] = useState(false);
+  useEffect(() => {
+    init()
+      .then(() => {
+        setDbInitialized(true);
+      })
+      .then(() => SplashScreen.hideAsync())
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  if (!dbInitialized) {
+    return null;
+  }
+
   return (
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-        headerStyle: { backgroundColor: Colors.primary500},
-        headerTintColor: Colors.gray700,
-        contentStyle: { backgroundColor: Colors.gray700},
-      }}>
-          <Stack.Screen 
-            name="AllPlaces" 
-            component={AllPlaces} 
-            options={({navigation}) => ({
-            title: "Your Favorite Places",
-            headerRight: ({ tintColor }) => (
-              <IconButton icon="add" size={24} color={tintColor} onPress={() => navigation.navigate("AddPlace")} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.primary500 },
+            headerTintColor: Colors.gray700,
+            contentStyle: { backgroundColor: Colors.gray700 },
+          }}
+        >
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              title: "Your Favorite Places",
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add"
+                  size={24}
+                  color={tintColor}
+                  onPress={() => navigation.navigate("AddPlace")}
+                />
               ),
-          })}
+            })}
           />
-          <Stack.Screen name="AddPlace" component={AddPlace} options={{
-            title: "Add a new Place"
-          }}/>
+          <Stack.Screen
+            name="AddPlace"
+            component={AddPlace}
+            options={{
+              title: "Add a new Place",
+            }}
+          />
           <Stack.Screen name="Map" component={Map} />
         </Stack.Navigator>
       </NavigationContainer>
@@ -43,8 +75,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
