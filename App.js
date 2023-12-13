0f510a927f28d9806a160e1081438472c2313632
BillStephens2022 <two4onebill@yahoo.com>
2023-12-13 08:16:47 -0500

    updated PlaceDetails screen to render details on individual place that has been selected

--- App.js
+++ App.js
@@ -67,7 +67,9 @@ export default function App() {
             }}
           />
           <Stack.Screen name="Map" component={Map} />
-          <Stack.Screen name="PlaceDetails" component={PlaceDetails} />
+          <Stack.Screen name="PlaceDetails" component={PlaceDetails} options={{
+            title: "Loading place...",
+          }} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
